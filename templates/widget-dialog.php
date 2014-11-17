8e96517866ef080185f6811b4a8d5a2a7cc7e1b1
Samer Bechara <sam@courtix.com>
2014-11-17 12:48:29 +0200

    Fixed issues with jQuery UI images not loading

--- templates/widget-dialog.php
+++ templates/widget-dialog.php
@@ -37,7 +37,7 @@ $ual = new UAL_Template();
 ?>
 
 <!-- Load jquery UI theme change to suit your needs-->
-<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css" />
+<link rel="stylesheet" type='text/css' href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
 <script type="text/javascript">
     
     jQuery(document).ready(function($) {
